5a29d67c17c54ebfb0b99f871199464c3800313c
xavierathlan <xavierathlan@hotmail.com>
2022-11-02 20:48:57 -0300

    first commit

---
+++ Entities/Contact.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ModuloApi.Entities
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+       
+        public string Nome { get; set; }
+
+        public string Telefone { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
